823fde6514d86e431508d4e7c9ecc6f955a5659b
Edivan Teixeira <edivanteixeira@gmail.com>
2018-08-02 16:04:37 -0300

    fix for review

--- src/Blockchain.js
+++ src/Blockchain.js
@@ -7,30 +7,30 @@ const SHA256 = require('crypto-js/sha256');
 module.exports = class BlockChain {
 
     constructor() {
-
+        (async () => {
+            this.currentHeight = (await this.getBlockHeight()) - 1;
+            this.addBlock(new Block("First block in the chain - Genesis block :)"));
+        }).bind(this)();
     }
+
     async addBlock(newBlock) {
-        let currentHeight = await this.getBlockHeight();
+        //this.currentHeight = await this.getBlockHeight();
+        this.currentHeight++;
+        newBlock.height = this.currentHeight;
         // UTC timestamp
         newBlock.time = new Date().getTime().toString().slice(0, -3);
         // previous block hash
-        if (currentHeight > 0) {
-            let previousBlock = await this.getBlock(currentHeight);
+        if (this.currentHeight > 0) {
+            let previousBlock = await this.getBlock(this.currentHeight - 1);
             newBlock.previousBlockHash = previousBlock.hash;
-            currentHeight++;
-        } else {
-            currentHeight = 1;
         }
-
-        newBlock.height = currentHeight;
         // Block hash with SHA256 using newBlock and converting to a string
         newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-
         // Adding block object to chain
         return await this.saveData(newBlock);
     }
-    
-    async saveData(newBlock){
+
+    async saveData(newBlock) {
         await db.put(newBlock.height, JSON.stringify(newBlock));
         return newBlock;
     }
@@ -41,7 +41,9 @@ module.exports = class BlockChain {
             db.createReadStream().on('data', function (data) {
                 i++;
             }).on('error', function (err) {
-                return console.log('Unable to read data stream!', err)
+                reject();
+                console.log('Unable to read data stream!', err)
+
             }).on('close', function () {
                 resolve(i);
             });
@@ -51,55 +53,52 @@ module.exports = class BlockChain {
 
 
     async getBlock(blockHeight) {
-        var blockJson = await db.get(blockHeight);
-        return JSON.parse(blockJson);
+
+        const block = new Promise((resolve, reject) =>
+            db.get(blockHeight, function (err, value) {
+                if (err) return console.log('Not found!', err);
+                resolve(JSON.parse(value))
+            })
+        )
+        return await block;
     }
 
 
-    validateBlock(block) {
+    async validateBlock(blockHeight) {
+
+        let block = await this.getBlock(blockHeight);
         // get block hash
         let blockHash = block.hash;
         let blockToCheck = Object.assign({}, block);
-
         // remove block hash to test block integrity
         blockToCheck.hash = '';
         // generate block hash
         let validBlockHash = SHA256(JSON.stringify(blockToCheck)).toString();
         // Compare
-        if (blockHash === validBlockHash) {
-            return true;
-        } else {
-            console.log('Block #' + block.height + ' invalid hash:\n' + blockHash + '<>' + validBlockHash);
-            return false;
-        }
+        return (blockHash === validBlockHash);
     }
 
-    async validateBlockByHeight(blockHeight) {
-        // get block object
-        let block = await this.getBlock(blockHeight);
-        return this.validateBlock(block);
-    }
 
 
 
     async validateChain() {
         let errorLog = [];
-        let maxtHeight = (await this.getBlockHeight() - 1);
 
-        for (var i = 0; i < maxtHeight; i++) {
+        //get current height
+        let height = await this.getBlockHeight();
 
-            let currentBlock = await this.getBlock(i + 1);
-            let nextBlock = await this.getBlock(i + 2);
-            let valid = this.validateBlock(currentBlock);
+        for (var i = 0; i < height; i++) {
+            //Check if current block is valid
+            var isValid = await this.validateBlock(i);
+            if (!isValid) {
+                errorLog.push(i);
+            }
+            if (i < (height - 1)) {
+                let currentBlock = await this.getBlock(i);
+                let nextBlock = await this.getBlock(i + 1);
 
-            if (!valid) errorLog.push(i + 1);
-
-
-            // compare blocks hash link
-            let blockHash = currentBlock.hash;
-            let previousHash = nextBlock.previousBlockHash;
-            if (blockHash !== previousHash) {
-                errorLog.push(i + 1);
+                if (nextBlock.previousBlockHash !== currentBlock.hash)
+                    errorLog.push(i);
             }
         }
         if (errorLog.length > 0) {
@@ -109,5 +108,4 @@ module.exports = class BlockChain {
             console.log('No errors detected');
         }
     }
-
 }
