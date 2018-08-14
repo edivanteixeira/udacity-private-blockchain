e6070f4b0c7de9c8eec606cc11a55dd33b296d2b
Edivan Teixeira <edivanteixeira@gmail.com>
2018-08-14 08:12:19 -0300

    Add information about express on README and validate post block

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ Blockchain has the potential to change the way that the world approaches data. D
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
 
-### Prerequisites
-
-Installing Node and NPM is pretty straightforward using the installer package available from the (Node.js® web site)[https://nodejs.org/en/].
+### Requirements
+* [Node.js](https://nodejs.org)
+* [NPM](https://www.npmjs.com)
+* [LevelDB](https://github.com/google/leveldb)
+* [ExpressJS](https://expressjs.com/)
 
 ### Running soluction
 
@@ -26,3 +28,16 @@ npm start
 ```
 - Before this, open a browser in [http://localhost:8000](http://localhost:8000) and enjoy :)
 
+#### Create Block
+```
+curl -X POST \
+  http://localhost:8000/block \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'body=teste!'
+```
+
+#### Get block by height
+```
+curl -X GET \
+  http://localhost:8000/block/0
+```
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var Block = require('../Modules/Blockchain/Block.js')
+const { body } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const { check, validationResult } = require('express-validator/check');
+
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.redirect('/api-docs')
 });
 
-router.post('/block/', (req, res, next) => {
+router.post('/block/', [
+  // body must be not empty
+  body('body').not().isEmpty().trim().escape()
+], (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errors: errors.array()
+    });
+  }
+
   Promise.resolve()
-    .then(() =>
-      blockchain.addBlock(new Block(req.body.body))
-    )
+    .then(() => {
+      if (req.body.body)
+        return blockchain.addBlock(new Block(req.body.body))
+    })
     .then((block) => {
       return res.json(block)
     })
